dabdbfe0366a167c61ee776c0b1bc25eb7fd357b
harryazariah <harryazariah@bitbucket.org>
2019-03-13 20:12:56 +0000

    lab 3

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+version = "1.23.0"
+subscription_id = "a822836f-a295-4355-8b42-3f8d950e4e10"
+tenant_id =  "dfe2b6f0-2602-4e7e-aedb-56ee085dd161"
+skip_provider_registration = true
+
+}
