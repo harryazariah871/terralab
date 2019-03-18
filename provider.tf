234931178a9101826eeeec2c80ca31c0da44ebeb
harryazariah <harryazariah@bitbucket.org>
2019-03-18 14:16:53 +0000

    working on providers

--- provider.tf
+++ provider.tf
@@ -1,8 +1,14 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
-  version         = "1.23.0"
-  subscription_id = "a822836f-a295-4355-8b42-3f8d950e4e10"
-  tenant_id       = "dfe2b6f0-2602-4e7e-aedb-56ee085dd161"
-  client_id       = "7c61752f-8f3c-4513-be41-fcdd33ba64de"
-  client_secret   = "d2f37480-d3dc-4db5-8017-a981892c0506"
+version = "1.23.0"
+subscription_id = "a822836f-a295-4355-8b42-3f8d950e4e10"
+tenant_id =  "dfe2b6f0-2602-4e7e-aedb-56ee085dd161"
+skip_provider_registration = true
+}
+
+provider "azurerm" {
+alias = "core"
+version = "1.23.0"
+subscription_id = "a822836f-a295-4355-8b42-3f8d950e4e10"
+tenant_id =  "dfe2b6f0-2602-4e7e-aedb-56ee085dd161"
 }
